359e54a78a27f88dd6f7ee10997ade7d99de8d44
SixPositionPro <494120176@qq.com>
2017-10-16 16:52:40 +0800

    all

--- www/config_default.py
+++ www/config_default.py
@@ -12,8 +12,6 @@ configs = {
     'db': {
         'host': '127.0.0.1',
         'port': 3306,
-        'user': 'root',
-        'password': '1234',
         'db': 'awesome'
     },
     'session': {
